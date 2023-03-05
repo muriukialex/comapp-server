2b02afae426452bb07a5b9acc9e3c9ea8b63b62d
alexmuriuki <alexmuriukimaina254@gmail.com>
2023-03-05 23:12:47 +0300

    rename schema/queryTypes.js to schema/query.js

--- schema/schema.js
+++ schema/schema.js
@@ -1,4 +1,4 @@
 const { GraphQLSchema } = require('graphql')
-const { RootQuery, mutation } = require('./queryTypes')
+const { RootQuery, mutation } = require('./query')
 
 module.exports = new GraphQLSchema({ query: RootQuery, mutation })
